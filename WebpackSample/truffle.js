728a1e6f2c52bfc3f3bf0e8de4abb32f1aa48d3d
ssfcultra <koolkeith13@gmail.com>
2017-11-18 13:54:29 -0800

    Added Truffle Webpack Demo

---
+++ WebpackSample/truffle.js
@@ -0,0 +1,18 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  networks: {
+  development: {
+  host: "localhost",
+  port: 8545,
+  network_id: "*" // Match any network id
+ },
+ production: {
+  host: "etherblfcalt.westus2.cloudapp.azure.com",
+  port: 8545,
+  network_id: "*", // Match any network id
+  gas: 4600000
+ }
+}
+};
